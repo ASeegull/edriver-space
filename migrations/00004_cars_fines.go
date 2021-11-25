2c81bea2f35a80fe51c9e20ee761b363f032b80d
mariareshetovska <mresetovska@gmail.com>
2021-11-25 18:56:12 +0200

    add migration car

---
+++ migrations/00004_cars_fines.go
@@ -0,0 +1,39 @@
+package migrations
+
+import (
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(UpCarsFines, DownCarsFines)
+}
+
+func UpCarsFines(tx *sql.Tx) error {
+	query := `CREATE TABLE IF NOT EXISTS cars_fines
+		(
+			vehicle_registration_number VARCHAR(25)  NOT NULL,
+            date_and_time               DATE         NOT NULL,
+            place                       VARCHAR(255) NOT NULL,
+            file_law_article            VARCHAR(30)  NOT NULL,
+            price                       INT          NOT NULL,
+            info                        VARCHAR(255),
+            img_id                      VARCHAR(255),
+            FOREIGN KEY (vehicle_registration_number) REFERENCES cars (registration_number)
+		);`
+	_, err := tx.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DownCarsFines(tx *sql.Tx) error {
+	query := `DROP TABLE IF EXISTS cars_fines;`
+	_, err := tx.Exec(query)
+	if err != nil {
+		return err
+	}
+	return nil
+}
