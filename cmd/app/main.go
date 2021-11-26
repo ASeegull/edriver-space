ce3fd19a25b111d7b82e14e61d10452dbe545e11
Arsen Nedovis <arsennedovis@gmail.com>
2021-11-26 22:22:29 +0200

    connection v3

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/ASeegull/edriver-space/api/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	// Path to env
+	if err := godotenv.Load("env/docker/postgres.env", "env/docker/app.env"); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
 	fmt.Println("Hello Lv-644.Go!")
 	s := server.NewServer()
-	s.Logger.Fatal(s.Start(":1323"))
+	s.Logger.Fatal(s.Start(":1323")) // Move to env .. hardcode port repot
+
+	// Verify if connection is ok
+	conn := MustGetConnection()
+	err := conn.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected ✓")
+	defer func() {
+		if err := conn.Close(); err != nil {
+			fmt.Println("db connection closed.")
+		}
+	}()
 }
