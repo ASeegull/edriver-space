2ca787e1e1befacca15765faaca51637000a3940
Arsen Nedovis <arsennedovis@gmail.com>
2021-12-01 15:05:17 +0200

    Update main.go
    conn v4

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,30 +5,44 @@ import (
 	"log"
 
 	"github.com/ASeegull/edriver-space/api/server"
+	"github.com/ASeegull/edriver-space/config"
+	"github.com/ASeegull/edriver-space/logger"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	fmt.Println("Hello Lv-644.Go!")
 
 	// Path to env
 	if err := godotenv.Load("env/docker/postgres.env", "env/docker/app.env"); err != nil {
 		log.Fatalf("error loading env variables: %s", err.Error())
 	}
 
-	fmt.Println("Hello Lv-644.Go!")
+	//Initializing Logger
+	logger.LogInit()
+
+	// Loading config values
+	conf, err := config.LoadConfig("")
+
+	if err != nil {
+		logger.LogErr(err)
+	}
+
+	//Creating and starting server
 	s := server.NewServer()
-	s.Logger.Fatal(s.Start(":1323")) // Move to env .. hardcode port repot
+	logger.LogFatal(s.Start(":" + conf.ServerPort))
 
 	// Verify if connection is ok
 	conn := MustGetConnection()
-	err := conn.Ping()
-	if err != nil {
-		log.Fatal(err)
+	conErr := conn.Ping()
+	if conErr != nil {
+		log.Fatal(conErr)
 	}
 	fmt.Println("Successfully connected ✓")
 	defer func() {
-		if err := conn.Close(); err != nil {
+		if conErr := conn.Close(); conErr != nil {
 			fmt.Println("db connection closed.")
 		}
 	}()
+
 }
