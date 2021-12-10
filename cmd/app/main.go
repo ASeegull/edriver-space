afd39e1dc6652e6feb3008e17bfa58ac38510e0a
ArturChopikian <artur4ik303@gmail.com>
2021-12-10 03:08:28 +0200

    rewrited main file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,56 +2,40 @@ package main
 
 import (
 	"fmt"
-	"github.com/ASeegull/edriver-space/api/server"
 	"github.com/ASeegull/edriver-space/config"
-	"github.com/ASeegull/edriver-space/logger"
+	"github.com/ASeegull/edriver-space/internal/server"
 	_ "github.com/ASeegull/edriver-space/migrations"
-	"github.com/ASeegull/edriver-space/storage"
-	"github.com/joho/godotenv"
-	"github.com/pressly/goose/v3"
-	"os"
+	"github.com/ASeegull/edriver-space/pkg/db/postrges"
+	"github.com/ASeegull/edriver-space/pkg/db/redis"
+	"log"
 )
 
 func main() {
 	fmt.Println("Hello Lv-644.Go!")
 
-	//Initializing Logger
-	logger.LogInit()
-
-	// Loading config values
-	conf, err := config.LoadConfig("")
-
+	cfgFile, err := config.LoadConfig("./config/config-local")
 	if err != nil {
-		logger.LogErr(err)
+		log.Fatalf("LoadConfig: %v", err)
 	}
 
-	// Path to env
-	if err := godotenv.Load("env/docker/postgres.env", "env/docker/app.env"); err != nil {
-		logger.LogFatal(fmt.Errorf("error loading env variables: %s", err.Error()))
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("ParseConfig: %v", err)
 	}
 
-	// Open db connection
-	conn := storage.MustGetConnection()
-
-	if conErr := conn.Ping(); conErr != nil {
-		logger.LogFatal(conErr)
+	db, err := postrges.NewPostgresDB(cfg)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
-	fmt.Println("Successfully connected ✓")
 
-	defer func() {
-		if conErr := conn.Close(); conErr != nil {
-			logger.LogFatal(err)
-		}
-	}()
+	//if err := goose.Up(db, "."); err != nil {
+	//	logger.LogFatal(err)
+	//}
 
-	if err := goose.SetDialect(os.Getenv("DB_DRIVER")); err != nil {
-		logger.LogFatal(err)
+	redisClient := redis.NewRedisClient(cfg)
+
+	s := server.NewServer(db, redisClient, cfg)
+	if err = s.Run(); err != nil {
+		log.Fatal(err.Error())
 	}
-	// Up new migrations
-	if err := goose.Up(conn, "."); err != nil {
-		logger.LogFatal(err)
-	}
-	//Creating and starting server
-	s := server.NewServer()
-	logger.LogFatal(s.Start(":" + conf.ServerPort))
 }
