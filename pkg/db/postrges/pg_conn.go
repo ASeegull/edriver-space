74733744101d015af7f173ff96a55c570eadcf45
ArturChopikian <artur4ik303@gmail.com>
2021-12-10 02:49:16 +0200

    set up connection to postgresql

---
+++ pkg/db/postrges/pg_conn.go
@@ -0,0 +1,29 @@
+package postrges
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/ASeegull/edriver-space/config"
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
+
+func NewPostgresDB(cfg *config.Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.Postgres.PostgresqlDriver,
+		cfg.Postgres.PostgresqlUser,
+		cfg.Postgres.PostgresqlPassword,
+		cfg.Postgres.PostgresqlHost,
+		cfg.Postgres.PostgresqlPort,
+		cfg.Postgres.PostgresqlDbname,
+		cfg.Postgres.PostgresqlSSLMode)
+
+	db, err := goose.OpenDBWithDriver(cfg.Postgres.PostgresqlDriver, dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
