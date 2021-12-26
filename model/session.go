d8ed9f20585ffd62234121a091d11819c52aa33f
ArturChopikian <artur4ik303@gmail.com>
2021-12-26 22:05:53 +0200

    combined models and model

--- model/session.go
+++ model/session.go
@@ -1,4 +1,4 @@
-package models
+package model
 
 type Session struct {
 	RefreshToken string
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	Id       string
-	Email    *string
-	Password *string
-	Role     *string
-}
