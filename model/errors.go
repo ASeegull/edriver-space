a823be47fd3fd62cc46b96fa5899b238f8a2bf8e
ArturChopikian <artur4ik303@gmail.com>
2022-01-02 18:19:58 +0200

    adding new error

--- model/errors.go
+++ model/errors.go
@@ -3,7 +3,8 @@ package model
 import "errors"
 
 var (
-	ErrUserNotFound    = errors.New("user doesn't exists")
-	ErrSessionNotFound = errors.New("session doesn't exists")
-	ErrWrongFileType   = errors.New("wrong file type")
+	ErrUserNotFound       = errors.New("user doesn't exists")
+	ErrSessionNotFound    = errors.New("session doesn't exists")
+	ErrWrongFileType      = errors.New("wrong file type")
+	ErrUserWithEmailExist = errors.New("the user with such email is registered")
 )
