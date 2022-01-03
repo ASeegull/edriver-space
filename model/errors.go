d9da966ec3e3336ac247f1375af7c6eb086293da
alex <anonim2771@gmail.com>
2022-01-03 14:38:24 +0200

    Add new error

--- model/errors.go
+++ model/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrUserNotFound    = errors.New("user doesn't exists")
 	ErrSessionNotFound = errors.New("session doesn't exists")
 	ErrWrongFileType   = errors.New("wrong file type")
+	ErrJWTEmpty        = errors.New("JWT token has empty body")
 )
