6342489fe7511f3c7be1d962d06bd026995bad0d
mariareshetovska <mresetovska@gmail.com>
2022-01-09 22:12:24 +0200

    add cars and drivers to handler

--- handler/handler.go
+++ handler/handler.go
@@ -23,21 +23,43 @@ type Uploader interface {
 	UploadExcel() echo.HandlerFunc
 }
 
+type Drivers interface {
+	InitDriversRoutes(e *echo.Group)
+	CreateDriver() echo.HandlerFunc
+	GetDriver() echo.HandlerFunc
+	GetDrivers() echo.HandlerFunc
+	DeleteDriver() echo.HandlerFunc
+}
+
+type Cars interface {
+	InitCarsRoutes(e *echo.Group)
+	CreateCar() echo.HandlerFunc
+	GetCar() echo.HandlerFunc
+	GetCars() echo.HandlerFunc
+	DeleteCar() echo.HandlerFunc
+}
+
 // Handlers stores all handlers
 type Handlers struct {
-	Users  Users
-	Upload Uploader
+	Users   Users
+	Upload  Uploader
+	Drivers Drivers
+	Cars    Cars
 }
 
 // NewHandlers returns a pointer to new Handlers
 func NewHandlers(services *service.Services, cfg *config.Config) *Handlers {
 	return &Handlers{
-		Users:  NewUsersHandlers(services.Users, cfg),
-		Upload: NewUploadHandler(services.Uploader, cfg),
+		Users:   NewUsersHandlers(services.Users, cfg),
+		Upload:  NewUploadHandler(services.Uploader, cfg),
+		Drivers: NewDriverHandlers(services.Drivers, cfg),
+		Cars:    NewCarsHandlers(services.Cars, cfg),
 	}
 }
 
 func (h *Handlers) InitRoutes(e *echo.Group, mw middleware.Middleware) {
 	h.Users.InitUsersRoutes(e, mw)
 	h.Upload.InitUploaderRoutes(e)
+	h.Drivers.InitDriversRoutes(e)
+	h.Cars.InitCarsRoutes(e)
 }
