38f2a3d20e16f851697a16b10c0806c3958dbeae
ArturChopikian <artur4ik303@gmail.com>
2022-01-17 10:45:31 +0200

    add new models for fines

---
+++ model/cars_fine.go
@@ -0,0 +1,12 @@
+package model
+
+type CarsFine struct {
+	Id                        string `json:"id"`
+	VehicleRegistrationNumber string `json:"vehicle_registration_number"`
+	DaraAndTime               string `json:"dara_and_time"`
+	Place                     string `json:"place"`
+	FileLawArticle            string `json:"file_law_article"`
+	Price                     int    `json:"price"`
+	Info                      string `json:"info"`
+	ImdUrl                    string `json:"imd_url"`
+}
