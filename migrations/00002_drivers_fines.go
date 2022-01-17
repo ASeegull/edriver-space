6169d20cbce93cd15b05a6496ad367c999a71c44
ArturChopikian <artur4ik303@gmail.com>
2022-01-17 11:04:17 +0200

    add id to 2 tables

--- migrations/00002_drivers_fines.go
+++ migrations/00002_drivers_fines.go
@@ -12,12 +12,13 @@ func init() {
 func UpUsersFines(tx *sql.Tx) error {
 	query := `CREATE TABLE IF NOT EXISTS drivers_fines
 		(
-			licence_number              VARCHAR(55)  NOT NULL,
-			date_and_time               DATE         NOT NULL,
-			place                       VARCHAR(255) NOT NULL,
-			file_law_article            VARCHAR(30)  NOT NULL,
-			price                       INT          NOT NULL,
-			vehicle_registration_number VARCHAR(25)  NOT NULL,
+		    id							 SERIAL,
+			licence_number               VARCHAR(55)  NOT NULL,
+			date_and_time                DATE         NOT NULL,
+			place                        VARCHAR(255) NOT NULL,
+			file_law_article             VARCHAR(30)  NOT NULL,
+			price                        INT          NOT NULL,
+			vehicle_registration_number  VARCHAR(25)  NOT NULL,
 			FOREIGN KEY (licence_number) REFERENCES drivers (licence_number)
 		);`
 	_, err := tx.Exec(query)
